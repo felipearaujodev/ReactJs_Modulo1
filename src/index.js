c6d9cef6167ee9622f63e4464df70ac6de677b85
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2019-03-19 08:22:21 -0300

    ReactJS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-alert("hello felipe");
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { render } from "react-dom";
+
+class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+  render() {
+    return (
+      <a href="" onClick={this.props.onClick} style={{ background: "#F00" }}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+class App extends Component {
+  handleClick() {
+    alert("Botão clicado");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello</h1>
+        <Button
+          onClick={() => {
+            alert("Button 1");
+          }}
+        />
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
